904af95802f981f807483331797b3a94e191852b
sergio <sfernandezrodriguez@danielcastelao.org>
2025-10-03 10:42:46 +0200

    ejercicio 4_4

---
+++ 4_4.py
@@ -0,0 +1,213 @@
+numero = int(input('digite el nombre del articulo: '))
+if numero > 0 and numero < 10:
+    if numero == 1:
+        print('uno')
+    if numero == 2:
+        print('dos')
+    if numero == 3:
+        print('tres')
+    if numero == 4:
+        print('cuatro')
+    if numero == 5:
+        print('cinco')
+    if numero == 6:
+        print('seis')
+    if numero == 7:
+        print('siete')
+    if numero == 8:
+        print('ocho')
+    if numero == 9:
+        print('nueve')
+if numero >= 10 and numero < 20:
+   if numero == 10:
+       print("diez")
+   if numero == 11:
+       print("once")
+   if numero == 12:
+       print("doce")
+   if numero == 13:
+       print("trece")
+   if numero == 14:
+       print("catorce")
+   if numero == 15:
+       print("quince")
+   if numero >= 16:
+        print('dieci')
+        if numero == 16:
+            print('seis')
+        if numero == 17:
+            print('siete')
+        if numero == 18:
+            print('ocho')
+        if numero == 19:
+            print('nueve')
+if numero >= 20 and numero < 30:
+    if numero == 20:
+        print("veinte")
+    else:
+        print('veinti')
+        if numero ==21:
+            print('uno')
+        if numero == 22:
+            print('dos')
+        if numero == 23:
+            print('tres')
+        if numero == 24:
+            print('cuatro')
+        if numero == 25:
+            print('cinco')
+        if numero == 26:
+            print('seis')
+        if numero == 27:
+            print('siete')
+        if numero == 28:
+            print('ocho')
+        if numero == 29:
+            print('nueve')
+if numero >= 30 and numero < 40:
+    print('treinta')
+    if numero == 31:
+        print('y uno')
+    if numero == 32:
+        print('y dos')
+    if numero == 33:
+        print('y tres')
+    if numero == 34:
+        print('y cuatro')
+    if numero == 35:
+        print('y cinco')
+    if numero == 36:
+        print('y seis')
+    if numero == 37:
+        print('y siete')
+    if numero == 38:
+        print('y ocho')
+    if numero == 39:
+        print('y nueve')
+
+if numero >= 40 and numero < 50:
+    print('cuarenta')
+    if numero ==41:
+        print('y uno')
+    if numero == 42:
+        print('y dos')
+    if numero == 43:
+        print('y tres')
+    if numero == 44:
+        print('y cuatro')
+    if numero == 45:
+        print('y cinco')
+    if numero == 46:
+        print('y seis')
+    if numero == 47:
+        print('y siete')
+    if numero == 48:
+        print('y ocho')
+    if numero == 49:
+        print('y nueve')
+
+
+if numero >= 50 and numero < 60:
+    print('cincuenta')
+    if numero ==51:
+        print('y uno')
+    if numero == 52:
+        print('y dos')
+    if numero == 53:
+        print('y tres')
+    if numero == 54:
+        print('y cuatro')
+    if numero == 55:
+        print('y cinco')
+    if numero == 56:
+        print('y seis')
+    if numero == 57:
+        print('y siete')
+    if numero == 58:
+        print('y ocho')
+    if numero == 59:
+        print('y nueve')
+if numero >= 60 and numero < 70:
+    print('sesenta')
+    if numero ==61:
+        print('y uno')
+    if numero == 62:
+        print('y dos')
+    if numero == 63:
+        print('y tres')
+    if numero == 64:
+        print('y cuatro')
+    if numero == 65:
+        print('y cinco')
+    if numero == 66:
+        print('y seis')
+    if numero == 67:
+        print('y siete')
+    if numero == 68:
+        print('y ocho')
+    if numero == 69:
+        print('y nueve')
+if numero >= 70 and numero < 80:
+    print('setenta')
+    if numero ==71:
+        print('y uno')
+    if numero == 72:
+        print('y dos')
+    if numero == 73:
+        print('y tres')
+    if numero == 74:
+        print('y cuatro')
+    if numero == 75:
+        print('y cinco')
+    if numero == 76:
+        print('y seis')
+    if numero == 77:
+        print('y siete')
+    if numero == 78:
+        print('y ocho')
+    if numero == 79:
+        print('y nueve')
+if numero >= 80 and numero < 90:
+    print('ochenta')
+    if numero ==81:
+        print('y uno')
+    if numero == 82:
+        print('y dos')
+    if numero == 83:
+        print('y tres')
+    if numero == 84:
+        print('y cuatro')
+    if numero == 85:
+        print('y cinco')
+    if numero == 86:
+        print('y seis')
+    if numero == 87:
+        print('y siete')
+    if numero == 88:
+        print('y ocho')
+    if numero == 89:
+        print('y nueve')
+if numero >= 90 and numero <= 100:
+    print('noventa')
+    if numero ==91:
+        print('y uno')
+    if numero == 92:
+        print('y dos')
+    if numero == 93:
+        print('y tres')
+    if numero == 94:
+        print('y cuatro')
+    if numero == 95:
+        print('y cinco')
+    if numero == 96:
+        print('y seis')
+    if numero == 97:
+        print('y siete')
+    if numero == 98:
+        print('y ocho')
+    if numero == 99:
+        print('y nueve')
+if numero == 100:
+    print('cien')
+if numero == 0:
+    print('cero')
